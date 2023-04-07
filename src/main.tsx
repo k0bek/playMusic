5e75646a812de516304035d2a823a3e0051e6b88
Kuba <kubakiebala@gmail.com>
2023-04-07 21:03:07 +0200

    feat: add spinner implementation for images loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AuthContextProvider } from "context/AuthContext";
+import { SongContextProvider } from "context/SongContext";
 import App from "App";
 import "main.scss";
-import { AuthContextProvider } from "context/AuthContext";
-import { SongContext, SongContextProvider } from "context/SongContext";
-import { LoginModal } from "feature/LoginModal/LoginModal";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
