b1ce410c8f2ed28a15f40395a41e07f3fff35d27
Kuba <kubakiebala@gmail.com>
2023-04-12 19:18:27 +0200

    fix types in input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,16 +1,18 @@
-import React, { forwardRef, InputHTMLAttributes } from "react";
+import { forwardRef } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
 import styles from "./Input.module.scss";
 
-type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-	register?: any;
-};
+interface InputProps {
+	type: string;
+	placeholder?: string;
+	register: UseFormRegisterReturn<"email" | "password" | "name">;
+}
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
 	({ register, ...rest }, ref) => {
 		return (
 			<input
 				className={styles.input}
-				ref={ref}
 				{...register}
 				{...rest}
 				onKeyDown={(event) => {
