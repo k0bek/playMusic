f23dbebb72d70f7d5e92890cce587d3a8aac7260
Kuba <kubakiebala@gmail.com>
2023-04-13 16:46:31 +0200

    delete

--- src/components/Layout/Layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Layout.module.scss";
-
-type ChildrenType = {
-	children: JSX.Element;
-};
-
-export const Layout = ({ children }: ChildrenType) => {
-	return <div className={styles.layout}>{children}</div>;
-};
--- src/components/Wrapper/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Wrapper.module.scss";
-
-type WrapperProps = {
-	children: JSX.Element;
-};
-
-export const Wrapper = ({ children }: WrapperProps) => {
-	return <div className={styles.wrapper}>{children}</div>;
-};
